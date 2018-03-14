ceafb7b58873f7ffde8c557e11ead9279fb2134c
carlosga <carlos.guzman.1@outlook.es>
2018-03-14 18:56:11 +0100

    · Updated gitlab ci configuration · Added a simple build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf build
+mkdir build && cd build
+# cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++-libc++ -DCMAKE_C_COMPILER=clang
+make -j4    
+# strace ./build/Debug/vulkan-sample
