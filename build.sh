dbaf9d84f07cd36cad31d55040e79e454ea84477
carlosga <carlos.guzman.1@outlook.es>
2019-07-09 16:07:39 +0200

    · Updated externals & build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 rm -rf build
 mkdir build && cd build
 # cmake .. -DCMAKE_BUILD_TYPE=Debug
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++-libc++ -DCMAKE_C_COMPILER=clang
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
 make -j4    
 # strace ./build/Debug/vulkan-sample
